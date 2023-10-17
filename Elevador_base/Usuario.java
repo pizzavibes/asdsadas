9716daee30417542f7416e5dcc6d188be15533e4
Santiago Álvarez <143003325+pizzavibes@users.noreply.github.com>
2023-10-16 20:49:44 -0500

    Add files via upload

---
+++ Elevador_base/Usuario.java
@@ -0,0 +1,23 @@
+package elevador;
+import java.util.Scanner;
+//piso, set y get
+public class Usuario {
+
+    private static int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+ private int piso_usuario;
+ 
+
+ public Usuario(int piso){
+ piso_usuario=piso;   
+
+Scanner lectura = new Scanner (System.in);
+
+ System.out.println("Ingrese su piso");
+
+     int piso_usuario = Usuario.nextInt(); 
+ }
+ 
+
+}
